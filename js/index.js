aee36b085d37ddd1f4464e62b621cb35e3cba4e1
Danita Codes <danitacodes@gmail.com>
2022-04-09 11:21:49 -0700

    display tasks in html

--- js/index.js
+++ js/index.js
@@ -1,19 +1,54 @@
-let task = new taskManager(0);
-
-document.querySelector('#taskForm').addEventListener('submit', (e) => {
-    //Get form values
-    const taskName = document.querySelector('#taskName').value;
-    const description = document.querySelector('#description').value;
-    const assignedTo = document.querySelector('#assignedTo').value;
-    const dueDate = document.querySelector('#Test_DatetimeLocal').value;
-
-    //Add tasks
-    taskManager.addTask(name, description, assignedTo, dueDate);
-
-     //Clear form values
-     document.querySelector('#taskName').value = '';
-     document.querySelector('#description').value = '';
-     document.querySelector('#assignedTo').value = '';
-     document.querySelector('#Test_DatetimeLocal').value = '';
-})
+const taskManager = new TaskManager(0);
+ 
+const taskForm = document.querySelector('#taskForm');
+ 
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+ 
+    const newTaskName = document.querySelector('#taskName');
+    const newDescription = document.querySelector('#description');
+    const newAssignedTo = document.querySelector('#assignedTo');
+    const newDueDate = document.querySelector('#Test_DatetimeLocal');
+ 
+    /*
+        Validation code here
+    */
+ 
+    const taskName = newTaskName.value;
+    const description= newDescription.value;
+    const assignedTo= newAssignedTo.value;
+    const dueDate = newDueDate.value;
+ 
+    taskManager.addTask(taskName, description, assignedTo, dueDate);
+ 
+    // Render the tasks
+    taskManager.render();
+ 
+    //Clear values after input
+    document.querySelector('#taskName').value = '';
+    document.querySelector('#description').value = '';
+    document.querySelector('#assignedTo').value = '';
+    document.querySelector('#Test_DatetimeLocal').value = '';
+});
+ 
+//Create inner html for tasks
+const createTaskHtml = (name, description, assignedTo, status, dueDate) => `
+<li class="list-group-item">
+    <div class="col-md-4 mb-3">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Task</h5>
+                <p id="id" class="card-text">Task Id</p>
+                <p class="card-text">Task Name: ${name}</p>
+                <p class="card-text">Task Description: ${description}</p>
+                <p class="card-text">Assigned To: ${assignedTo}</p>
+                <p class="card-text">Status: ${status}</p>
+                <p class="card-text">Due Date: ${dueDate}</p>
+                <a href="#" class="btn mr-2"><i class="fa-solid fa-pen-to-square"></i></a>
+                <a href="#" class="btn "><i class="fa-solid fa-trash"></i></a>
+            </div>
+        </div>
+    </div>
+</li>
+ `
 
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,12 +1,12 @@
 //Class to manage tasks
-class taskManager {
+class TaskManager {
     constructor(currentId = 0) {
         this.tasks = [];
         this.currentId = currentId;
     }
 
-    //Adds tasks
-    addTask(name, description, assignedTo, dueDate) {
+    //Adds task method
+    addTask(name, description, assignedTo, dueDate, status) {
         //Get UI elements
 
         const task = {
@@ -19,5 +19,34 @@ class taskManager {
         };
         
         this.tasks.push(task);
+    }
+
+    //Render tasks
+    render() {
+        //Create tasks array
+        const tasksHtmlList = [];
+
+        //Loop over tasks
+        for (let i = 0; i < this.tasks.length; i++) {
+            const task = this.tasks[i];
+
+            //Date formating
+            const date = new Date(task.dueDate);
+            const formattedDate = date.toDateString();
+
+            //Create html for task
+            const taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, task.status, formattedDate);
+
+            //Add tasks to array
+            tasksHtmlList.push(taskHtml);
+        }
+
+        //Create tasksHtml
+        const tasksHtml = tasksHtmlList.join('\n');
+
+        //Create inner html for task
+        const tasksList = document.querySelector('#tasksList');
+        tasksList.innerHTML = tasksHtml;
+
     }
 }
